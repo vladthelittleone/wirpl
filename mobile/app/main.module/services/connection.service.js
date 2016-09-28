eb9a9d3d8664bd77ec7f2185cecc64fe016d0433
vaimer <overespada@gmail.com>
2016-09-28 16:42:36 +0300

    my small change

---
+++ mobile/app/main.module/services/connection.service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Connection.$inject = ['$http'];
+
+module.exports = Connection;
+
+/**
+ * Ссылки на REST API
+ */
+var links = {
+
+    login:    '/login',
+    vkAuth:   '/vk/callback'
+};
+
+/**
+ * Created by vaimer on 28.09.16.
+ */
+
+function Connection($http) {
+
+  var that = {};
+
+  return that;
+
+  function
+}
